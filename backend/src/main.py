99f5188d60d46974fe038d2dd290d326ae1cf1b2
paty-oliveira <patriciia.mota@gmail.com>
2025-08-13 18:08:55 +0100

    Implement rag pipeline

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,38 +1,29 @@
 import os
-from parser import PDFParser
 
 from dotenv import load_dotenv
 
-from indexer import Indexer
 from llama_cloud_service import LlmaCloudService
-from query_engine import QueryEngine
+from rag import parsing_and_indexing_documents, query_documents
+
+LLAMA_CLOUD_API = os.getenv("LLAMA_CLOUD_API_KEY")
+
+
+def main():
+    pdf_files = [
+        os.path.abspath("pdfs/novo_regime.pdf"),
+        os.path.abspath("pdfs/perguntas_frequentes.pdf"),
+    ]
+
+    llama_service = LlmaCloudService(api_key=LLAMA_CLOUD_API)
+    index_name = "self_employed_documents"
+    parsing_and_indexing_documents(pdf_files, index_name, llama_service)
+
+    query = "What is the new regime for self-employed workers?"
+    response = query_documents(query, index_name, llama_service)
+    print(f"Query: {query}")
+    print(f"Response: {response}")
+
 
 if __name__ == "__main__":
     load_dotenv()
-    LLAMA_CLOUD_API = os.getenv("LLAMA_CLOUD_API_KEY")
-
-    if not LLAMA_CLOUD_API:
-        raise ValueError("LLAMA_API_KEY environment variable is not set.")
-
-    llama_service = LlmaCloudService(api_key=LLAMA_CLOUD_API)
-
-    # pdf_files = [
-    #     os.path.abspath("pdfs/novo_regime.pdf"),
-    #     os.path.abspath("pdfs/perguntas_frequentes.pdf"),
-    # ]
-
-    # pdf_parser = PDFParser(file_paths=pdf_files, parser_service=llama_service, result_type="text")
-    # documents_payload = pdf_parser.parse()
-
-    index_name = "example_index"
-    index_service = Indexer(index_name=index_name, index_service=llama_service)
-
-    # for document in documents_payload:
-    #     index_service.index(document["file_content"])
-    #     print(f"Indexed document: {document["file_name"]} in index: {index_name}")
-
-    query = "What is the new regime for self-employed workers?"
-    query_engine = QueryEngine(query_text=query, search_engine=llama_service, index_name=index_name)
-    response = query_engine.query()
-
-    print(f"Query Response: {response}")
+    main()
---
+++ backend/src/rag.py
@@ -0,0 +1,36 @@
+from parser import PDFParser
+
+from indexer import Indexer
+from query_engine import QueryEngine
+
+
+def parsing_and_indexing_documents(pdf_files, index_name, external_service):
+    """
+    Parses PDF documents and indexes their content using the provided external service.
+    Args:
+        pdf_files (list): List of paths to PDF files to be parsed.
+        index_name (str): Name of the index where the parsed content will be stored.
+        external_service: An instance of the external service used for parsing and indexing.
+    """
+
+    pdf_parser = PDFParser(file_paths=pdf_files, parser_service=external_service, result_type="text")
+    documents_payload = pdf_parser.parse()
+
+    index_service = Indexer(index_name=index_name, index_service=external_service)
+
+    for document in documents_payload:
+        index_service.index(document["file_content"])
+        print(f"Indexed document: {document['file_name']} in index: {index_name}")
+
+
+def query_documents(query_text, index_name, external_service):
+    """
+    Queries the indexed documents using the provided query text.
+    Args:
+        query_text (str): The text to query against the indexed documents.
+        index_name (str): The name of the index to search in.
+        external_service: An instance of the external service used for querying.
+    """
+    query_engine = QueryEngine(query_text=query_text, search_engine=external_service, index_name=index_name)
+    response = query_engine.query()
+    return response
