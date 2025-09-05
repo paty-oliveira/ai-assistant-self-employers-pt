a745fc7553e0f841168e91c220e7aec152b055e6
paty-oliveira <patriciia.mota@gmail.com>
2025-09-05 18:06:08 +0100

    Add minimal chat-bot conversation

--- frontend/app.py
+++ frontend/app.py
@@ -1,11 +1,39 @@
-import streamlit as st
 import requests
+import streamlit as st
 
-st.title("AI-Assistant for Self-Employeers in Portugal")
+API_URL = "http://api:80"
+ENDPOINT = "/query"
+
+
+def query_ai_assistant(prompt):
+    try:
+        response = requests.post(
+            API_URL + ENDPOINT,
+            json={"query": prompt, "index_name": "rag_index_test"},
+            headers={"Content-Type": "application/json"},
+        )
+
+        if response.status_code == 200:
+            data = response.json()
+            return data["response"]
+
+        else:
+            return "Not able to get a response from the AI assistant."
+
+    except requests.exceptions.RequestException:
+        return f"Error: {response.status_code}, {response.text}"
+
+
+def generate_stream(response):
+    for word in response.split(" "):
+        yield word + " "
+
 
 # Initialize session state for chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
+
+st.title("AI-Assistant for Self-Employeers in Portugal")
 
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
@@ -15,7 +43,16 @@ for message in st.session_state.messages:
 if prompt := st.chat_input("Ask me a question!"):
     st.session_state.messages.append({"role": "user", "content": prompt})
 
+    # Display user message in chat message container
     with st.chat_message("user"):
         st.markdown(prompt)
+
+    # Get response from AI assistant
+    with st.spinner("AI Assistant is getting your answer..."):
+        response = query_ai_assistant(prompt)
+
+    stream = generate_stream(response)
+    with st.chat_message("assistant"):
+        st.write_stream(stream)
 
     st.session_state.messages.append({"role": "assistant", "content": response})
